f1b6d1680ac9ba3c35109d7b3eb78654fd659e4b
Saif Ali <saifali7646@gmail.com>
2022-01-11 10:24:35 -0500

    add material ui icon to Topbar component

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,5 +1,8 @@
 import "./topbar.css"
 import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Chat';
 
 export default function Topbar(){
   return (
@@ -19,6 +22,21 @@ export default function Topbar(){
           <span className="topbarLink">Timeline</span>
         </div>
       </div>
+      <div className="topbarIcons">
+        <div className="topbarIconItem">
+          <PersonIcon />
+          <span className="topbarIconBadge">1</span>
+        </div>
+        <div className="topbarIconItem">
+          <ChatIcon />
+          <span className="topbarIconBadge">2</span>
+        </div>
+        <div className="topbarIconItem">
+          <NotificationsIcon />
+          <span className="topbarIconBadge">1</span>
+        </div>
+      </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
     </div>
   )
 }
